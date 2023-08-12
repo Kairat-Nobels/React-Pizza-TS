c456221f2a57844fd10596ef014e820d854842ca
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-08-12 21:19:10 +0600

    done cartFunctional

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './pizzaBlock.css'
 import { useEffect } from 'react'
-function PizzaBlock({ title, price, imageUrl, sizes, types })
+import { useDispatch, useSelector } from 'react-redux'
+import { addCart } from '../../redux/slices/cartSlice'
+function PizzaBlock({ id, title, price, imageUrl, sizes, types })
 {
-    const [count, setCount] = useState(0)
     const [activeType, setActiveType] = useState(0)
     const [activeSize, setActiveSize] = useState(sizes[0] || 0)
     const typeNames = ['тонкое', 'традиционное']
+    const { items } = useSelector(state => state.cart)
+    const dispatch = useDispatch()
+    const onClickAdd = () =>
+    {
+        const item = { id, title, price, imageUrl, type: typeNames[activeType], size: activeSize }
+        dispatch(addCart(item))
+    }
     return (
         <div className='pizza-wrapper'>
             <div className="pizza-block">
@@ -24,12 +32,13 @@ function PizzaBlock({ title, price, imageUrl, sizes, types })
                 </div>
                 <div className="pizza-block__bottom">
                     <div className="pizza-block__price">от {activeSize === 30 ? price + 50 : activeSize === 40 ? price + 100 : price} сом</div>
-                    <button onClick={e => { setCount(count + 1) }} className="button button--outline button--add">
+                    <button onClick={onClickAdd} className="button button--outline button--add">
                         <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="white" />
                         </svg>
                         <span>Добавить</span>
-                        <i>{count}</i>
+                        {items.find(obj => obj.id === id)?.count > 0 &&
+                            <i>{items.find(obj => obj.id === id)?.count}</i>}
                     </button>
                 </div>
             </div>
